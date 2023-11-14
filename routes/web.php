9bb9a8799128f0c4dee6ec346038c3f0a4bb3b30
nirobhasan-git <papon.nirob@gmail.com>
2023-11-14 18:33:52 +0600

    Laravel Mail Send With Database Queue and Jobs

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    \Mail::to('nirob@gmail.com')->send(new \App\Mail\SendTestMail());
+    // \Mail::to('nirob@gmail.com')->send(new \App\Mail\SendTestMail());
+    dispatch(new \App\Jobs\SendMailJobs());
     return view('welcome');
 });
